6d3e0c3573cb0f3819bcd70692181760c0a0c552
Olaf Alders <olaf@wundersolutions.com>
2015-01-24 11:38:20 -0500

    Rename CLI script.

--- bin/github-mergevelocity.pl
+++ bin/github-mergevelocity.pl
@@ -3,6 +3,9 @@
 use strict;
 use warnings;
 
+# PODNAME: github-mergevelocity.pl
+# ABSTRACT: CLI for determining how quickly a pull request might get merged
+
 use lib 'lib';
 
 use Github::MergeVelocity;
