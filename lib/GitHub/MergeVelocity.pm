18cfdb26f3122b9c3b831c54cd5501e67c833f15
Olaf Alders <olaf@wundersolutions.com>
2015-01-25 11:58:10 -0500

    Add pitifully lacking docs.

--- lib/GitHub/MergeVelocity.pm
+++ lib/GitHub/MergeVelocity.pm
@@ -184,4 +184,28 @@ __END__
 
 =pod
 
+=head1 SYNOPSIS
+
+    use strict;
+    use warnings;
+
+    use GitHub::MergeVelocity;
+
+    my $velocity = GitHub::MergeVelocity->new(
+        url => [
+            'https://github.com/neilbowers/PAUSE-Permissions',
+            'https://github.com/oalders/html-restrict',
+        ]
+    );
+
+    my $report = $velocity->report;
+
+    $velocity->print_report; # prints a tabular report
+
+=head1 CAVEATS
+
+This module cannot (yet) distinguish between pull requests which were closed
+because they were rejected and pull requests which were closed because the
+patches were applied outside of GitHub's merge mechanism.
+
 =cut
